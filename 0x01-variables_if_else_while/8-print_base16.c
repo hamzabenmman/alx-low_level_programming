331481b26e3e78df159a0879bf01ec83632510c4
hamzabenmman <42034378+hamzabenmman@users.noreply.github.com>
2023-03-17 15:47:12 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,13 +12,9 @@ int main(void)
 	char low;
 
 	for (d = 0; d <= 9; d++)
-	  {
-	    putchar(d);
-	  }
+	  { putchar(d); }
 	for (low = 'a'; low <= 'f'; low++)
-	  {
-	    putchar(low);
-	  }
+	  { putchar(low); }
 	    putchar('\n');
 
 	return (0);
