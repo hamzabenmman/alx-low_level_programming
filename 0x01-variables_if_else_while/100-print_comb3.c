12f14f9c601074e816a19849f414ad57a68a66ff
hamzabenmman <42034378+hamzabenmman@users.noreply.github.com>
2023-03-17 18:17:15 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,14 +8,16 @@
  */
 int main(void)
 {
-	int i, j;
-       for (i = 0; i < 10; i++) {
-        for (j = i + 1; j < 10; j++) {
-            putchar('0' + i);
-            putchar('0' + j);
-            putchar(' ');
-	    putchar(','); 
-	 } 
-      }
+int i, j;
+ for (i = 0; i < 10; i++) 
+ {
+  for (j = i + 1; j < 10; j++) 
+   {
+    putchar('0' + i);
+    putchar('0' + j);
+    putchar(' ');
+    putchar(','); 
+   } 
+ }
     return 0;
 }
